49d4082cb4a50afced4927df6ffc5aa41b487965
Emily Shoji <eshoji1@lion.lmu.edu>
2017-08-02 16:23:08 -0700

    added jquery

--- About.js
+++ About.js
@@ -1,9 +1,9 @@
-function fadeOutElement(){
+function animateHeader(){
   $(h1).animate({fontSize: '30px'}, 2000)
 }
 
 function setup(){
-  $(h1).click(fadeOutElement());
+  $(h1).click(animateHeader());
 }
 
 $(document).ready(setup);
