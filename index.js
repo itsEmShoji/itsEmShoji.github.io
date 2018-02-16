3be84f8eb989cc39dd2b734d83ef44b853a1ecef
Emily Shoji <eshoji1@lion.lmu.edu>
2018-02-15 22:29:41 -0800

    updated everything

---
+++ index.js
@@ -0,0 +1,10 @@
+// function animateHeader(){
+//   $('h1').animate({fontSize: "200px"}, 2000)
+//   $('h1').animate({fontSize: "100px"}, 2000)
+// }
+//
+// function setup(){
+//   $('h1').click(animateHeader());
+// }
+//
+// $(document).ready(setup);
